893d90b70d792c5f18d6863a6a133b8b5b160694
henriquefsa98 <60986807+henriquefsa98@users.noreply.github.com>
2023-07-18 02:37:17 +0000

    falta testar eslint e melhorar hover do mouse

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+function apontador() {
+    const description = document.querySelector(".tooltip");
+    if (description instanceof HTMLElement) {
+        document.querySelectorAll('path').forEach((el) => el.addEventListener('mouseover', (event) => {
+            if (event.target instanceof Element) {
+                //event.target.className = ("enabled");
+                //event.target.setAttribute("class", "enabled");
+                description.classList.add("active");
+                description.innerHTML = event.target.id;
+                //console.log("mouseover");
+            }
+        }));
+        document.querySelectorAll('path').forEach((el) => el.addEventListener("mouseout", () => {
+            description.classList.remove("active");
+            //console.log("mouseout");
+        }));
+        document.onmousemove = function (e) {
+            description.style.left = e.pageX + "px";
+            description.style.top = (e.pageY - 70) + "px";
+            //console.log("mouse move");
+        };
+    }
+}
+function main() {
+    apontador();
+}
+main();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-
 function apontador(){
-
-
 
     const description = document.querySelector(".tooltip");
 
-    if(description){
+    if(description instanceof HTMLElement){
     
         document.querySelectorAll('path').forEach((el) =>
             el.addEventListener('mouseover', (event) => {
-                if(event instanceof HTMLElement){
-                    event.target.className = ("enabled");
+                if(event.target instanceof Element){
+                    //event.target.className = ("enabled");
+                    //event.target.setAttribute("class", "enabled");//faz com que os estados visitados fiquem azul...
                     description.classList.add("active");
                     description.innerHTML = event.target.id;
+                    //console.log("mouseover");
                 }
             })
 
@@ -21,16 +20,22 @@ function apontador(){
         document.querySelectorAll('path').forEach((el) =>
             el.addEventListener("mouseout", () => {
                 description.classList.remove("active");
+                //console.log("mouseout");
             })
         );
 
         document.onmousemove = function (e) {
+            
             description.style.left = e.pageX + "px";
             description.style.top = (e.pageY - 70) + "px";
+            //console.log("mouse move");
+            
         }
     }
 
 }
+
+
 function main(){
     apontador();
 }
